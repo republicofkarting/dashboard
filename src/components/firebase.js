fe31ebae3fe20fb96707684d2605d44a2a1b5192
aman282571 <aman282571@gmail.com>
2022-03-05 14:55:16 +0530

    final comit

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/compat/app";
 import "firebase/compat/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD9WbEZnb7uDGZnWq0-2xJHjeyCUqgJgTY",
-  authDomain: "dashboard-4d2ce.firebaseapp.com",
-  projectId: "dashboard-4d2ce",
-  storageBucket: "dashboard-4d2ce.appspot.com",
-  messagingSenderId: "897279111761",
-  appId: "1:897279111761:web:545e93c26f4d94cc24fe70",
+  apiKey: "AIzaSyBF-doDpIGGubgD-DYQAxrx5Q9Slpc-HUg",
+  authDomain: "dashboard-31891.firebaseapp.com",
+  databaseURL: "https://dashboard-31891-default-rtdb.firebaseio.com",
+  projectId: "dashboard-31891",
+  storageBucket: "dashboard-31891.appspot.com",
+  messagingSenderId: "1019639081570",
+  appId: "1:1019639081570:web:1abc6768d107f8ac163f3b",
 };
 
 // Initialize Firebase
